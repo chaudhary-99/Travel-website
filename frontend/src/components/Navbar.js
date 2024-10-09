ab8c523a55ae224b59b6b1496ba622fb1b0bc99e
chaudhary-99 <karishma.21b0101190@abes.ac.in>
2024-10-09 22:31:44 +0530

    tailwind-css

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,37 +1,65 @@
 import React, { useState } from 'react';
-import styles from './Navbar.module.css';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
-const Navbar = () => {
+const Navbar = ({ isAuthenticated }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  // Close menu when a link is clicked
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  // Determine if the current path is '/packages'
+  const isPackagesPage = location.pathname === '/packages' || location.pathname === '/contactus' ||  location.pathname === '/about';
+
   return (
-    <header className={styles.navbar}>
-      <div className={styles.logo}>Travel.</div>
-      <nav className={isOpen ? styles.open : ''}>
-        <ul>
-          <Link to='/'><li>Home</li></Link>
-          <li>Packages</li>
-          <li>About</li>
-          <li>Pages</li>
-          <li>Contact</li>
+    <header className={`flex justify-between items-center p-4 ${isPackagesPage? 'fixed':'absolute'} w-full top-0 z-10 transition duration-300 ${isPackagesPage ? 'bg-white' : ''}`}>
+      <div className={`text-2xl font-bold transition-transform transform hover:scale-110 text-orange-600`}>
+        <Link to="/" onClick={closeMenu}>Travel.</Link>
+      </div>
+      <nav className={`md:flex ${isOpen ? 'flex' : 'hidden'} flex-col md:flex-row md:items-center absolute md:static top-full md:top-0 left-0 w-full md:w-auto transition-all duration-500 ${isPackagesPage ? 'bg-white' : ' bg-transparent backdrop-blur-md'}`}>
+        <ul className="flex flex-col md:flex-row md:space-x-8 space-y-4 md:space-y-0 font-semibold text-lg md:text-base p-4 md:p-0 justify-center items-center">
+          <li className={`cursor-pointer transition-colors hover:text-orange-500 ${isPackagesPage ? 'text-gray-800' : 'text-white'}`}>
+            <Link to="/" onClick={closeMenu}>Home</Link>
+          </li>
+          <li className={`cursor-pointer transition-colors hover:text-orange-500 ${isPackagesPage ? 'text-gray-800' : 'text-white'}`}>
+            <Link to="/packages" onClick={closeMenu}>Packages</Link>
+          </li>
+          <li className={`cursor-pointer transition-colors hover:text-orange-500 ${isPackagesPage ? 'text-gray-800' : 'text-white'}`}>
+            <Link to="/about" onClick={closeMenu}>About</Link>
+          </li>
+          <li className={`cursor-pointer transition-colors hover:text-orange-500 ${isPackagesPage ? 'text-gray-800' : 'text-white'}`}>
+            <Link to="/contactus" onClick={closeMenu}>Contact</Link>
+          </li>
         </ul>
       </nav>
-      <div>
-        <Link to='signup'>
-          <button className={styles.signUp}>Sign Up</button>
+      <div className="flex items-center">
+      {isAuthenticated ? (
+          <Link to='/profile' onClick={closeMenu}>
+            <FontAwesomeIcon icon={faUserCircle} className="text-white text-3xl cursor-pointer hover:text-orange-500 transition-colors" />
+          </Link>
+        ) : (
+          <Link to='/signup' onClick={closeMenu}>
+            <button className={`font-semibold text-base ${isPackagesPage ? 'text-orange-500' : 'text-white'} border-2 border-orange-500 py-2 px-6 rounded-full hover:bg-orange-500 hover:text-white transition-colors duration-300`}>Sign Up</button>
+          </Link>
+        )}
+        <Link to="/book" onClick={closeMenu}>
+          <button className={`ml-4 font-semibold text-base bg-gradient-to-r from-orange-500 to-orange-600 text-white py-3 px-6 rounded-full hover:scale-105 transform transition-transform duration-300 hidden md:inline`}>
+            BOOK NOW
+          </button>
         </Link>
-        <button className={styles.bookNow}>BOOK NOW</button>
       </div>
-      
-      <div className={styles.hamburger} onClick={toggleMenu}>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
+      <div className="md:hidden flex flex-col cursor-pointer" onClick={toggleMenu} aria-label="Toggle menu">
+        <div className={`w-6 h-[3px] bg-gray-900 mb-1 transition-all ${isOpen ? 'rotate-45 translate-y-1.5' : ''}`}></div>
+        <div className={`w-6 h-[3px] bg-gray-900 mb-1 transition-all ${isOpen ? 'opacity-0' : ''}`}></div>
+        <div className={`w-6 h-[3px] bg-gray-900 transition-all ${isOpen ? '-rotate-45 -translate-y-1.5' : ''}`}></div>
       </div>
     </header>
   );
--- frontend/src/components/PopularDestinations.js
+++ frontend/src/components/PopularDestinations.js
@@ -1,24 +1,25 @@
-import React, { useRef } from 'react';
-import styles from './PopularDestinations.module.css';
+import React, { useEffect, useRef, useState } from 'react';
 import PopularDestinationsCard from './cards/PopularDestinationsCard';
-import bankeBihari from '../images/bankeBihari.jpg';
-import chandraodayaMandir from '../images/chandraodayaMandir.jpg';
-import premMandir from '../images/premMandir.jpg';
-import tajMahal from '../images/tajMahal.jpg';
-
-const destinations = [
-    { id: 1, name: 'Banke Bihari', image: bankeBihari },
-    { id: 2, name: 'Chandraodaya Mandir', image: chandraodayaMandir },
-    { id: 3, name: 'Prem Mandir', image: premMandir },
-    { id: 4, name: 'Taj Mahal', image: tajMahal },
-    { id: 5, name: 'Banke Bihari', image: bankeBihari },
-    { id: 6, name: 'Chandraodaya Mandir', image: chandraodayaMandir },
-    { id: 7, name: 'Prem Mandir', image: premMandir },
-    { id: 8, name: 'Taj Mahal', image: tajMahal }
-];
 
 const PopularDestinations = () => {
     const scrollContainerRef = useRef(null);
+    const [destinations, setDestinations] = useState([]);
+
+    useEffect(() => {
+        // Fetch popular destinations from backend
+        const fetchDestinations = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/popular-destinations'); // Adjust URL if needed
+                const data = await response.json();
+                setDestinations(data);
+                console.log(data)
+            } catch (error) {
+                console.error("Error fetching destinations:", error);
+            }
+        };
+
+        fetchDestinations();
+    }, []);
 
     const scroll = (direction) => {
         if (scrollContainerRef.current) {
@@ -32,24 +33,28 @@ const PopularDestinations = () => {
     };
 
     return (
-        <div className={styles.container}>
-            <div className={styles.upper}>
-                <div className={styles.heading}>
-                    <div className={styles.title}>Popular Destinations</div>
-                    <div className={styles.subtitle}>From historical cities to natural spectaculars, come see the best of the world!</div>
+        <div className="px-5 mb-20">
+            <div className="flex justify-between">
+                <div>
+                    <div className="pl-[3.1vw] text-4xl font-bold">Popular  Destinations</div>
+                    <div className="pl-[3.1vw] text-lg text-gray-500">From historical cities to natural spectaculars, come see the best of the world!</div>
                 </div>
-                <div className={styles.navigation}>
-                    <div className={styles.arrow} onClick={() => scroll('left')}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z" /></svg>
+                <div className="flex items-center hidden lg:flex">
+                    <div className="h-9 cursor-pointer mr-[15px]" onClick={() => scroll('left')}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="h-full hover:scale-110 hover:fill-gray-500 transition-transform duration-300">
+                            <path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z" />
+                        </svg>
                     </div>
-                    <div className={styles.arrow} onClick={() => scroll('right')}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM281 385c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l71-71L136 280c-13.3 0-24-10.7-24-24s10.7-24 24-24l182.1 0-71-71c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L393 239c9.4 9.4 9.4 24.6 0 33.9L281 385z" /></svg>
+                    <div className="h-9 cursor-pointer" onClick={() => scroll('right')}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="h-full hover:scale-110 hover:fill-gray-500 transition-transform duration-300">
+                            <path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM281 385c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l71-71L136 280c-13.3 0-24-10.7-24-24s10.7-24 24-24l182.1 0-71-71c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L393 239c9.4 9.4 9.4 24.6 0 33.9L281 385z" />
+                        </svg>
                     </div>
                 </div>
             </div>
 
-            <div className={styles.cardsContainer} ref={scrollContainerRef}>
-                <div className={styles.cards}>
+            <div className="w-full overflow-x-scroll no-scrollbar" ref={scrollContainerRef}>
+                <div className="flex mt-10 w-max">
                     {destinations.map(dest => (
                         <PopularDestinationsCard key={dest.id} destination={dest} />
                     ))}
